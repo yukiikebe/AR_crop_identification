5ccbc9f352c9fdad5045c8607d258aa6da79ebbd
Khoa Ho Viet Vo <vhvkhoa@gmail.com>
2024-08-03 03:03:50 -0500

    inference code

--- data/Arkansas/pre-processing.py
+++ data/Arkansas/pre-processing.py
@@ -1,4 +1,5 @@
 import xarray as xr
+import pickle as pkl
 import numpy as np
 import glob
 from skimage import exposure
@@ -10,16 +11,51 @@ import cv2
 import re
 from datetime import datetime
 
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
 
 
 SELECTED_BANDS = {
     "10m": ["B2", "B3", "B4", "B8"], # 10m resolution
     "20m": ["B5", "B6", "B7", "B8A", "B11", "B12"], # 20m resolution
-    "60m": ["B1", "B9", "QA60"], # 60m resolution
     "SCL": ["SCL"], # 20m resolution
 }
 
-def create_splits(img, input_shape, pad_mode="reflect"):
+def scl_mapping(img, data_dir, date):
+    # Define the colors in HEX and convert them to RGB
+    hex_colors = [
+        "#ff0004",  # Saturated or defective
+        "#868686",  # Dark Area Pixels
+        "#774b0a",  # Cloud Shadows
+        "#10d22c",  # Vegetation
+        "#ffff52",  # Bare Soils
+        "#0000ff",  # Water
+        "#818181",  # Clouds Low Probability / Unclassified
+        "#c0c0c0",  # Clouds Medium Probability
+        "#f1f1f1",  # Clouds High Probability
+        "#bac5eb",  # Cirrus
+        "#52fff9"   # Snow / Ice
+    ]
+
+    # Convert HEX to RGB
+    colors = [tuple(int(h[i:i+2], 16) / 255.0 for i in (1, 3, 5)) for h in hex_colors]
+
+    # Create a ListedColormap
+    cmap = ListedColormap(colors)
+
+    # Map the array values to the color map
+    mapped_array = cmap(img)  # Normalize array values to the range 0-1
+
+    # Display the image
+    filename = f'{data_dir}/scl_{date}.png'  # Replace this with the desired filename
+    plt.imsave(filename, mapped_array)
+
+    print(f"Image saved as {filename}")
+
+
+
+def create_splits(imgs, input_shape, pad_mode="reflect"):
     # A function to create splits out of a particular size from a given image.
     # images are split up row wise, i.e - row1 split up, row2 split up and so on
     # NOTE - padding is added in case the image can't be split into equal parts
@@ -28,12 +64,12 @@ def create_splits(img, input_shape, pad_mode="reflect"):
     splits = []
 
     # calculate pad length  
-    pad_len_y = (0 - img.shape[0]) % input_shape[0]  
-    pad_len_x = (0 - img.shape[1]) % input_shape[1]
+    pad_len_y = (0 - imgs.shape[1]) % input_shape[0]  
+    pad_len_x = (0 - imgs.shape[2]) % input_shape[1]
 
     # option to pad the xarray
-    if isinstance(img, xr.DataArray):
-        img = img.pad(pad_width={
+    if isinstance(imgs, xr.DataArray):
+        imgs = imgs.pad(pad_width={
             "y": (0, pad_len_y), 
             "x": (0, pad_len_x)}, 
             mode=pad_mode
@@ -41,22 +77,25 @@ def create_splits(img, input_shape, pad_mode="reflect"):
 
     # option to pad normal numpy array
     else:
-        img = np.pad(
-            img, 
-            [(0, pad_len_y), (0, pad_len_x), (0, 0)], 
+        imgs = np.pad(
+            imgs, 
+            [(0, 0), (0, pad_len_y), (0, pad_len_x), (0, 0)], 
             pad_mode
         )
 
     # loop through the indices with a step of input_shape
     # and create spltis for each index
-    for i in range(0, img.shape[0], input_shape[0]):
-        for j in range(0, img.shape[1], input_shape[1]):
+    patch_ids = []
+    for i in range(0, imgs.shape[1], input_shape[0]):
+        for j in range(0, imgs.shape[2], input_shape[1]):
           # use array slicing to select split size from the whole image
-          splits.append(img[i : i + input_shape[0], j : j + input_shape[1]].copy())
-    print("Done: create_splits  ", len(splits), img.shape)
-    return splits, img
+          splits.append(imgs[:, i : i + input_shape[0], j : j + input_shape[1]].copy())
+          patch_ids.append((i, j))
+    print("Done: create_splits  ", len(splits), imgs.shape)
+    return splits, patch_ids
 
-def stack_bands(satellite_image, cdl_image)->dict:
+
+def stack_bands(satellite_image)->dict:
     """
     Stack satellite bands and cdl image
     Args:
@@ -65,102 +104,23 @@ def stack_bands(satellite_image, cdl_image)->dict:
     Returns:
     dict : dictionary of stacked image
     """
-    stacked_image = {}
+    # It's the default order of Sentinel-2 bands (B01 to B12) except that B01,B09, and B10 are removed.
 
-    
-    # band 10m
-    B2_image = satellite_image["10m"]["B2"]
-    h10,w10 = B2_image["image"].shape #(1908, 2842)
-    x10 = B2_image["x"]
-    y10 = B2_image["y"]
+    stacked_image = np.dstack([
+        satellite_image['10m']['B2'],
+        satellite_image['10m']['B3'],
+        satellite_image['10m']['B4'],
+        satellite_image['20m']['B5'],
+        satellite_image['20m']['B6'],
+        satellite_image['20m']['B7'],
+        satellite_image['10m']['B8'],
+        satellite_image['20m']['B8A'],
+        satellite_image['20m']['B11'],
+        satellite_image['20m']['B12'],
+        satellite_image['SCL']['SCL'],
+    ])
 
-    SCL_image = satellite_image["SCL"]["SCL"]["image"] #(954, 1421)
-    upsampled_SCL_image = cv2.resize(SCL_image, (w10,h10), interpolation=cv2.INTER_NEAREST) #(1908, 2842)
-
-    stacked_image_10m = np.dstack([
-        satellite_image["10m"]["B2"]["image"],
-        satellite_image["10m"]["B3"]["image"],
-        satellite_image["10m"]["B4"]["image"],
-        satellite_image["10m"]["B8"]["image"],
-        upsampled_SCL_image,
-        cdl_image, #(1908, 2842)
-        ]) #(1908, 2842, 6)
-    
-    stacked_image["10m"] = xr.DataArray(
-        stacked_image_10m,
-        dims=['y', 'x', 'bands'], 
-        coords={
-            'y': y10[:, 0],  # y coordinates of each pixel
-            'x': x10[0, :],
-            'bands': list(range(stacked_image_10m.shape[2]))
-
-        })
-    
-
-    # band 20m
-    stacked_image_20m = np.dstack([
-        satellite_image["20m"]["B5"]["image"],
-        satellite_image["20m"]["B6"]["image"],
-        satellite_image["20m"]["B7"]["image"],
-        satellite_image["20m"]["B8A"]["image"],
-        satellite_image["20m"]["B11"]["image"],
-        satellite_image["20m"]["B12"]["image"],
-        ]) #(954, 1421, 6)
-
-    x20 = satellite_image["20m"]["B5"]["x"]
-    y20 = satellite_image["20m"]["B5"]["y"]
-    stacked_image["20m"] = xr.DataArray(
-        stacked_image_20m,
-        dims=['y', 'x', 'bands'], 
-        coords={
-            'y': y20[:, 0],  # y coordinates of each pixel
-            'x': x20[0, :],
-            'bands': list(range(stacked_image_20m.shape[2]))
-
-        })
-    
-    # band 60m
-    stacked_image["60m"] = np.dstack([
-        satellite_image["60m"]["B1"]["image"],
-        satellite_image["60m"]["B9"]["image"],
-        satellite_image["60m"]["QA60"]["image"]]) #(318, 475, 3)
-    x60 = satellite_image["60m"]["B1"]["x"]
-    y60 = satellite_image["60m"]["B1"]["y"]
-    stacked_image["60m"] = xr.DataArray(
-        stacked_image["60m"],
-        dims=['y', 'x', 'bands'], 
-        coords={
-            'y': y60[:, 0],  # y coordinates of each pixel
-            'x': x60[0, :],
-            'bands': list(range(stacked_image["60m"].shape[2]))
-
-        })
-    
     return stacked_image
-
-def tiling_image(image_stacked)->List:
-    """
-    Tiling image
-    Args:
-    image_stacked : dict : dictionary of stacked image
-    Returns:
-    list : list of tiled image
-    """
-    tiled_image = {}
-    
-    N = 24 #@param {type:"number"}
-    splits_10, padded_img_10 = create_splits(image_stacked["10m"], [N, N])
-    N = 12 #@param {type:"number"}
-    splits_20, padded_img_20 = create_splits(image_stacked["20m"], [N, N])
-    N = 4
-    splits_60, padded_img_60 = create_splits(image_stacked["60m"], [N, N])
-
-    if len(splits_10) != len(splits_20) or len(splits_20) != len(splits_60):
-        raise ValueError("Number of splits of 10m, 20m and 60m are not equal")
-    tiled_image["10m"] = splits_10
-    tiled_image["20m"] = splits_20
-    tiled_image["60m"] = splits_60
-    return tiled_image
 
 
 def read_satellite_image(data_dir,date)->dict:
@@ -172,30 +132,35 @@ def read_satellite_image(data_dir,date)->dict:
     Returns:
     dict : dictionary of satellite image
     """
-    statellite_image = {}
+    satellite_image = {}
     for resolution, bands in SELECTED_BANDS.items():
-        statellite_image[resolution] = {}
-        # print("bands: ",bands)
+        satellite_image[resolution] = {}
         for band in bands:
-            # print("band: ",band)
             file_path = os.path.join(data_dir,date, f"{band}_{date}.tif")
             with rasterio.open(file_path) as src:
-                # image[band] = src.read(1)
-                image = src.read(1)
-                width = src.width  # Width of the raster in pixels
-                height = src.height
-                # print("image shape: ",image.shape)
-                # print("width: ",width)
-                # print("height: ",height)
-                # exit()
-                transform = src.transform # Affine transform of the raster
-                x, y = np.meshgrid(np.arange(width), np.arange(height))
-                x, y = (x * transform[0]) + transform[2], (y * transform[4]) + transform[5]
-                statellite_image[resolution][band] = {}
-                statellite_image[resolution][band]["image"] = image
-                statellite_image[resolution][band]["x"] = x
-                statellite_image[resolution][band]["y"] = y
-    return statellite_image
+                data = src.read(1)
+                satellite_image[resolution][band] = data
+                #print(resolution, band, data.dtype, data.shape, data.min(), data.max())
+                if band == 'SCL':
+                    occlusion_ratio = np.sum(data > 7) / float(data.size)
+                    print(np.around(occlusion_ratio, 4))
+                    if  occlusion_ratio > 0.1:
+                        return None
+                    #scl_mapping(data, data_dir, date)
+
+    height_10m, width_10m = satellite_image['10m']['B2'].shape
+    for resolution, bands in SELECTED_BANDS.items():
+        for band in bands:
+            if resolution == '10m':
+                assert satellite_image[resolution][band].shape == (height_10m, width_10m)
+            else:
+                orig_im = np.copy(satellite_image[resolution][band])
+                upsampled_im = cv2.resize(orig_im, (width_10m, height_10m), interpolation=cv2.INTER_NEAREST) #(1908, 2842)
+                assert upsampled_im.shape == (height_10m, width_10m)
+                satellite_image[resolution][band] = upsampled_im
+
+    return satellite_image
+
 
 def read_cdl_image(data_dir,date):
     """
@@ -206,12 +171,13 @@ def read_cdl_image(data_dir,date):
     Returns:
     cdl image
     """
-
-    file_path = os.path.join(data_dir,f"rgb_{date}_cdl.tif")
+    file_path = os.path.join(data_dir, f"rgb_{date}_cdl.tif")
     with rasterio.open(file_path) as src:
         cdl_image = src.read(1)
         num_bands = src.count
     return cdl_image
+
+
 def read_series_satellite_and_cdl(satellite_image_dir:str, cdl_image_dir:str)->dict:
     """
     Read series of satellite images
@@ -225,27 +191,39 @@ def read_series_satellite_and_cdl(satellite_image_dir:str, cdl_image_dir:str)->d
     dates = os.listdir(satellite_image_dir)
     sorted_dates = sorted(dates, key=lambda date: datetime.strptime(date, "%Y-%m-%d"))
 
-    print("list dates: ",sorted_dates)
+    print(f"No. dates: {len(sorted_dates)}")
+    print(sorted_dates)
 
     # satellite_images = {}
-    tiled_images = {}
+    stacked_images, doys = [], []
     for date in sorted_dates:
-        print("Reading image at date: ",date)
+        print(f"Reading image at date: {date}")
         satellite_images = read_satellite_image(satellite_image_dir,date)
-        cdl_image = read_cdl_image(cdl_image_dir,"2023-03-02")
-        stacked_image = stack_bands(satellite_images,cdl_image)
-        tiled_image = tiling_image(stacked_image)
-        tiled_images[date] = tiled_image
+        if satellite_images is None:
+            continue
+        cdl_image = read_cdl_image(cdl_image_dir, "2023-03-02")
+        stacked_image = stack_bands(satellite_images)
+        stacked_images.append(stacked_image)
+        doys.append(datetime.strptime(date, '%Y-%m-%d').timetuple().tm_yday)
 
-        
-    return tiled_images
+    N = 24
+    stacked_images = np.stack(stacked_images, axis=0)
+    tiled_images, patch_ids = create_splits(stacked_images, (N, N))
+    return tiled_images, patch_ids, doys
 
 if __name__ == "__main__":
 
-    satellite_image_dir = "/home/vuonghn/research/dataset/satellite/arkansas/satellite_images"
-    cdl_image_dir = "/home/vuonghn/research/dataset/satellite/arkansas/org_maral/cdl"
-    list_statellite_image = read_series_satellite_and_cdl(satellite_image_dir,cdl_image_dir)
-    print(list_statellite_image.keys())
+    satellite_image_dir = "/home/khoavo/Desktop/workplace/satelite/raw_arkansas/"
+    cdl_image_dir = "/home/khoavo/Desktop/workplace/satelite/cdl_arkansas"
+    tiled_satellite_series, patch_ids, doys = read_series_satellite_and_cdl(satellite_image_dir,cdl_image_dir)
+    for series, patch_idx in zip(tiled_satellite_series, patch_ids):
+        pkl_data = {
+            'img': series,
+            'doy': doys,
+            'labels': np.zeros((1, 24, 24), dtype=np.uint8),
+        }
+        with open(f'/home/khoavo/Desktop/workplace/satelite/arkansas/{patch_idx[0]}_{patch_idx[1]}.pickle', 'wb') as f:
+            pkl.dump(pkl_data, f)
     # date = "2023-01-03"
     # statellite_image = read_satellite_image(data_dir,date)
     # print(statellite_image.keys())
