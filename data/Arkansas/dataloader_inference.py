cd26243a79af7b9adc9d9089ffe76ff993757491
vvuonghn <vvuonghn@gmail.com>
2024-10-27 22:15:13 -0500

    vvuonghn: fix code in infer

--- data/Arkansas/dataloader_inference.py
+++ data/Arkansas/dataloader_inference.py
@@ -3,58 +3,48 @@ import os
 import torch
 import numpy as np
 from torch.utils.data import Dataset
-import torch.utils.data
 import pickle
 import warnings
-warnings.filterwarnings("ignore")
 import yaml
-import numpy as np
 
-
+warnings.filterwarnings("ignore")
 
 # Load the classes for mapping the labels
 with open('configs/Arkansas/arkansas_data.yaml', 'r') as file:
     arkansas_data = yaml.safe_load(file)
 arkansas_classes = arkansas_data['classes']
-# Create a mapping from original labels to normalized labels
-label_mapping = {}
-normalized_label = 0
-
-for category in arkansas_classes.values():
-    for original_label in category.keys():
-        label_mapping[original_label] = normalized_label
-        normalized_label += 1
-
+class_mappings = {key: list(value.keys()) for key, value in arkansas_classes.items()}
 
 def normalize_classes(labels):
     """
     Normalize the labels based on the provided dictionary.
     """
     normalized_labels = torch.zeros_like(labels)
-    for original_label, norm_label in label_mapping.items():
-        normalized_labels[labels == original_label] = norm_label
-    return normalized_labels
-
+    for new_class, original_classes in class_mappings.items():
+        mask = torch.isin(labels, torch.tensor(list(original_classes)))
+        normalized_labels[mask] = new_class
+    return normalized_labels.int()
 
 def get_distr_dataloader(paths_file, root_dir, rank, world_size, transform=None, batch_size=32, num_workers=4,
                          shuffle=True, return_paths=False):
     """
-    return a distributed dataloader
+    Return a distributed dataloader.
     """
-    dataset = SatImDataset(csv_file=paths_file, root_dir=root_dir, transform=transform, return_paths=return_paths)
+    dataset = SatImDataset(data_paths=paths_file, root_dir=root_dir, transform=transform, return_paths=return_paths)
     sampler = torch.utils.data.distributed.DistributedSampler(dataset, num_replicas=world_size, rank=rank)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers,
                                              pin_memory=True, sampler=sampler)
     return dataloader
 
-
 def get_dataloader(paths, root_dir, transform=None, batch_size=32, num_workers=4, shuffle=True,
                    return_paths=False, my_collate=None):
+    """
+    Return a dataloader.
+    """
     dataset = SatImDataset(data_paths=paths, root_dir=root_dir, transform=transform, return_paths=return_paths)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers,
                                              collate_fn=my_collate)
     return dataloader
-
 
 class SatImDataset(Dataset):
     """Satellite Images dataset."""
@@ -62,10 +52,11 @@ class SatImDataset(Dataset):
     def __init__(self, data_paths, root_dir, transform=None, multilabel=False, return_paths=False):
         """
         Args:
-            csv_file (string): Path to the csv file with annotations.
+            data_paths (list): List of paths to the data files.
             root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
+            transform (callable, optional): Optional transform to be applied on a sample.
+            multilabel (bool, optional): Whether the dataset is multilabel.
+            return_paths (bool, optional): Whether to return the paths of the images.
         """
         self.data_paths = data_paths
         self.root_dir = root_dir
@@ -82,17 +73,15 @@ class SatImDataset(Dataset):
 
         img_name = os.path.join(self.root_dir, self.data_paths[idx])
         with open(img_name, 'rb') as handle:
-            sample = pickle.load(handle, encoding='latin1') # (['img', 'labels', 'doy'])
+            sample = pickle.load(handle, encoding='latin1')  # (['img', 'labels', 'doy'])
             if sample['img'].shape[-1] == 11:
                 sample['img'] = sample['img'][..., :-1]
                 sample['img'] = np.transpose(sample['img'].astype(np.float32), (0, 3, 1, 2))
-            original_labels = sample['labels']
+        
         if self.transform:
-            sample = self.transform(sample)  #dict_keys
+            sample = self.transform(sample)
         
         sample["labels"] = normalize_classes(sample["labels"])
-
-    
         
         if self.return_paths:
             return sample, img_name.split('/')[-1].split('.')[0]
